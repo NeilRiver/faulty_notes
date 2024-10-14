914059dfef05986940ff65c4c41c19ca563d4165
NeilRiver <nelo-001@mail.ru>
2024-10-14 17:18:52 +0300

    update homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <div>Егор</div>;
+  return (
+    <div>
+      <section className="text-white p-10 bg-cyan-500 min-h-[470px]">
+        <header className="flex flex-col items-center">
+          <h1 className="text-5xl">Travel Note</h1>
+          <p className="text-xl p-5">
+            Store your travel experience! Open new horizons!
+          </p>
+          <Button className="uppercase text-sm">Get Started</Button>
+        </header>
+      </section>
+    </div>
+  );
 }
